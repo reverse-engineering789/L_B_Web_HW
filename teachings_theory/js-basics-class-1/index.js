87c9c405ccc0f6c5b5354326d40ceb2afcdf76c6
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2023-08-12 23:22:04 +0530

    hello world

--- teachings_theory/js-basics-class-1/index.js
+++ teachings_theory/js-basics-class-1/index.js
@@ -1,3 +1,6 @@
+// objects and static values for functions
+
+
 // let rectangle = { 
 //         length: 1,
 //         breadth:2,
@@ -13,3 +16,17 @@
 // ---------------------------------------------------------
 
 
+function createRectangle(){
+    
+    return rectangle = {
+        length:1,
+        length:2,
+
+        draw(){
+            console.log("Hello world");
+        }
+    };
+}
+
+let rectangleObj1 = createRectangle();
+console.log(rectangleObj1); 
