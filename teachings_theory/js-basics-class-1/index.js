e73db72226a6b5200ff8a2f97dbff9eb5c6e1cd5
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2023-08-12 23:04:21 +0530

    going going

---
+++ teachings_theory/js-basics-class-1/index.js
@@ -0,0 +1,15 @@
+// let rectangle = { 
+//         length: 1,
+//         breadth:2,
+
+//         draw(){
+//             console.log("my name");
+//         }
+//    }
+
+//    console.log(rectangle.draw());
+
+
+// ---------------------------------------------------------
+
+
