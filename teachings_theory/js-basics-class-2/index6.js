f1322f4641e1c9b82328868cefd28bf2524f4af8
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2023-08-13 12:44:07 +0530

    hellow

--- teachings_theory/js-basics-class-2/index6.js
+++ teachings_theory/js-basics-class-2/index6.js
@@ -27,12 +27,30 @@
 
 
 
-let a = 10;
+// let a = 10;
+
+// function inc(a){
+//     a++;
+// }
+
+// inc(a);
+
+// console.log(a);
+
+
+// // ------------------------------------------------------------
+
+
+
+let a = {value:10};
 
 function inc(a){
-    a++;
+    a.value++;
 }
 
 inc(a);
 
-console.log(a);
+console.log(a.value);
+
+// Output:
+// 11
---
+++ teachings_theory/js-basics-class-2/index7.js
@@ -0,0 +1,23 @@
+// keys - for in
+
+        // for of
+
+let rectangle = {
+    length:2,
+    breadth: 4
+};
+
+
+for (let key in rectangle){
+    console.log(key,rectangle[key]);
+}
+
+// Output:
+// length 2
+// breadth 4]
+
+
+// -------------------------------------------------------------
+
+
+
