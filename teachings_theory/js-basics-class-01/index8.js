1a15703a7368f481f787dd2e2cec917a0aed52ff
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2023-08-21 10:56:57 +0530

    hello world

---
+++ teachings_theory/js-basics-class-01/index8.js
@@ -0,0 +1,32 @@
+// Ternary Operator
+
+let age = 18;
+
+let ans = age >= 18 ? true : false;
+
+console.log(ans);
+
+
+// Output:
+// true
+
+
+
+// ----------------------------------------------------
+
+
+let age2 = 17;
+let ans2 = age2 >= 18 ? true : false;
+
+console.log(ans2);
+
+// Output:
+// false
+
+
+
+// ----------------------------------------------------
+
+
+
+
---
+++ teachings_theory/js-basics-class-01/index9.js
@@ -0,0 +1,59 @@
+// logical operators:
+
+// AND 
+// OR 
+// NOT
+
+
+let age = 24;
+
+if (age >= 18 && age <= 24){
+    console.log("fit");
+}else{
+    console.log("unfit");
+}
+// Output:
+// fit
+
+
+// --------------------------------------------
+
+
+
+let age2 = 44;
+
+if (age2 >= 18 && age2 <= 24){
+    console.log("fit");
+}else{
+    console.log("unfit");
+}
+// Output:
+// unfit
+
+
+
+// ------------------------------------------------
+
+
+
+
+let fname = "Delhi";
+
+if (fname === "Jaipur" || fname === "Delhi"){
+    console.log("interested");
+}else{
+    console.log("not interested");
+}
+
+// Output:
+// interested
+
+
+
+
+// ---------------------------------------------------
+
+
+
+
+
