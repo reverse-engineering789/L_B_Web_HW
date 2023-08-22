35bfc845be7247d82bb8a1cfb450129eea277420
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2023-08-22 16:24:52 +0530

    index

---
+++ teachings_theory/js-basics-class-03/index2.js
@@ -0,0 +1,67 @@
+// strings 
+
+console.log("hi");
+// Output:
+// hi
+
+
+
+let lastName = 'abc';
+let firstName = new String('def');
+
+console.log(typeof("abc"));
+console.log(typeof(firstName));
+// Output:
+// string
+// object
+
+
+console.log(lastName.length);
+console.log(lastName[0]);
+
+// Output:
+// 3
+// a
+
+
+console.log(lastName.includes('ac'));
+// Output:
+// false
+
+console.log(lastName.includes('ab'));
+// Output:
+// true
+
+
+console.log(lastName.startsWith('ab'));
+// Output:
+// true
+
+console.log(lastName.endsWith('c'));
+// Output:
+// true
+
+
+console.log(lastName.toUpperCase());
+// Output:
+// ABC
+
+
+
+console.log(lastName.toLowerCase());
+// Output:
+// abc
+
+
+
+console.log(lastName.trim());
+// Output:
+// abc
+
+console.log(lastName.replace('ab','ef'));
+// Output:
+// efc
+
+
+
+
