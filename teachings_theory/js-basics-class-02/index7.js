59423cd444ff50404552c3b79c6f6f7b183f6b8c
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2023-08-22 10:35:05 +0530

    js - 2

--- teachings_theory/js-basics-class-02/index7.js
+++ teachings_theory/js-basics-class-02/index7.js
@@ -21,3 +21,37 @@ for (let key in rectangle){
 
 
 
+// for of loop(only used for iterable values like arrays)
+            
+// for(let key of rectangle){
+//     console.log(key);
+// }
+// // Output:
+// // TypeError: rectangle is not iterable
+
+
+
+console.log("--------------------------")
+
+
+
+for (let key of Object.keys(rectangle)){
+    console.log(key);
+}
+
+// Output:
+// length
+// breadth
+
+console.log("--------------------------")
+
+
+
+
+for (let key of Object.entries(rectangle)){
+    console.log(key);
+}
+
+// Output:
+// [ 'length', 2 ]
+// [ 'breadth', 4 ]
--- teachings_theory/js-basics-class-02/index8.js
+++ teachings_theory/js-basics-class-02/index8.js
@@ -1,1 +1,104 @@
-// object cloning 
+// // object cloning  
+
+// // object cloning  #1
+// let src = {
+//     a: 10,
+//     b: 20,
+//     c: 30 
+// };
+
+// let dest = {};
+
+// for (let key in src){
+//     dest[key] = src[key];
+// }
+
+// console.log(dest);
+
+// src.a++;
+// console.log(dest);
+
+
+// // Output:
+// // { a: 10, b: 20, c: 30 }
+// // { a: 10, b: 20, c: 30 }
+
+
+
+// // ---------------------------------
+
+// // object cloning  #2
+
+
+
+// let src = {
+//     a: 10,
+//     b: 20,
+//     c: 30
+// };
+
+// let dest = Object.assign({}, src);
+
+// console.log(dest);
+
+// src.a++;
+
+// console.log(dest);
+
+// // Output:
+// // { a: 10, b: 20, c: 30 }
+// // { a: 10, b: 20, c: 30 }
+
+
+
+// // ---------------------------------
+
+
+// // adding another sorce 
+
+
+// let src = {
+//     a: 10,
+//     b: 20,
+//     c: 30
+// };
+
+// let src2 = {value: 25};
+
+// let dest = Object.assign({}, src, src2);
+
+// console.log(dest);
+
+// src.a++;
+
+// console.log(dest);
+
+// // Output:
+// // { a: 10, b: 20, c: 30, value: 25 }
+// // { a: 10, b: 20, c: 30, value: 25 }
+
+
+// // -----------------------------------------
+
+
+
+// // object cloning  #3
+
+
+let src = {
+    a: 10,
+    b: 20,
+    c: 30
+};
+
+let dest = {...src};
+
+console.log(dest);
+
+src.a++;
+
+console.log(dest);
+
+// Output:
+// { a: 10, b: 20, c: 30 }
+// { a: 10, b: 20, c: 30 }
