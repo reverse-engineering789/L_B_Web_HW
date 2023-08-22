dc0d4d2ccf45bb05575a4bf231701417f690a227
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2023-08-22 11:13:04 +0530

    index

--- teachings_theory/js-basics-class-03/index1.js
+++ teachings_theory/js-basics-class-03/index1.js
@@ -1,4 +1,32 @@
-// 1:10:10
-// callback functions
+// Math 
 
 
+console.log(Math.random());
+
+console.log(Math.PI);
+
+console.log(Math.round(1.8));
+
+// Output:
+// 0.526349676349859
+// 3.141592653589793
+// 2
+
+
+console.log(Math.max(2,4,6));
+// Output:
+// 6
+
+
+console.log(Math.min(2,4,6));
+// Output:
+// 2
+
+
+console.log(Math.abs(2));
+// Output:
+// 2
+
+console.log(Math.abs(-2));
+// Output:
+// 2
