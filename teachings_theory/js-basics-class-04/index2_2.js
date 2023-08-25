73e73f69453ddec6f8f65216fc4987c35608dbb0
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2023-08-25 17:28:42 +0530

    hello world

--- teachings_theory/js-basics-class-04/index2_2.js
+++ teachings_theory/js-basics-class-04/index2_2.js
@@ -16,23 +16,30 @@
 
 
 
-function sum(a, b){
-    let total = 0;
-    for(let value of arguments){
-        total = total + value;
-        }
-    return total;
-    }
+// function sum(a, b){
+//     let total = 0;
+//     for(let value of arguments){
+//         total = total + value;
+//         }
+//     return total;
+//     }
 
 
-let ans = sum(1,2,3,4,5,6);
-console.log(ans);
+// let ans = sum(1,2,3,4,5,6);
+// console.log(ans);
 
-// Output:
-// 21
+// // Output:
+// // 21
 
 
-// //--------------------------------------------------------
+// // --------------------------------------------------------
+
+
+
+
+
+
+
 
 
 
--- teachings_theory/js-basics-class-04/index8.js
+++ teachings_theory/js-basics-class-04/index8.js
@@ -1,1 +1,42 @@
 // Reducing an array:
+
+
+// let arr = [1,2,3,4];
+// let total = 0;
+
+// for(let value of arr){
+//     total = total + value;
+// }
+// console.log(total);
+
+
+// Output:
+// 10
+
+
+// ------------------------------------
+
+
+
+// let array2 = [-1,-2,-3,-4];
+
+// let totalSum = array2.reduce((accumulator, currentValue) => accumulator + currentValue,0);
+// console.log(`printing total sum: ${totalSum}`);
+
+// // Output:
+// // printing total sum: -10
+
+
+// ----------------------------------------------------
+
+
+
+
+let array3 = [-1,-2,-3,-5];
+
+let totalSum3 = array3.reduce((accumulator, currentValue) => accumulator + currentValue);
+console.log(`printing total sum: ${totalSum3}`);
+
+
+ // Output:
+ // printing total sum: -11
